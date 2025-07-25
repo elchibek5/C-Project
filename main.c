38e0b6551bd127937671d2e0aef82a5d639a3df3
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-07-24 22:09:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+// Function to calculate the square of a number
+int square(int n) {
+    return n * n;
+}
+
+// Function to check if a number is even
+int is_even(int n) {
+    return n % 2 == 0;
+}
+
 int main(void)
 {
-    printf("Hello World/n");
+    printf("Hello World\n");
+
+    int num = 5;
+    printf("Square of %d is %d\n", num, square(num));
+
+    if (is_even(num)) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+
+    return 0;
 }
